ec2e24f7abeb2a557c2802679e73add12a3cc17b
Marco Mendes <corelio@gmail.com>
2016-12-06 14:04:19 -0200

    Calculadora DevOps

---
+++ src/main/java/com/arkhi/pocdevops/Calculadora.java
@@ -0,0 +1,20 @@
+package com.arkhi.pocdevops;
+
+public class Calculadora {
+	public static double soma(double a, double b) {
+		return a + b;
+	}
+
+	public static double multiplica(double a, double b) {
+		return a * b;
+	}
+
+	public static double subtrai(double a, double b) {
+		return a - b;
+	}
+
+	public static double divide(double a, double b) {
+		return 0; // DEVE SER IMPLEMENTADO;
+	}
+
+}
